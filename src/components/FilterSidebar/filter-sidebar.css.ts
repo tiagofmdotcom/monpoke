be9d4a4060099085f3522db3e71572b32c0af64b
Tiago FM <tmarques@broadvoice.com>
2026-04-28 15:05:46 +0100

    ui tweak

--- src/components/FilterSidebar/filter-sidebar.css.ts
+++ src/components/FilterSidebar/filter-sidebar.css.ts
@@ -7,12 +7,12 @@ export default css`
     align-self: start;
     max-height: calc(100vh - 32px);
     overflow-y: auto;
+    border-radius: 12px;
+    border: 1px solid #ddd;
   }
 
   .sidebar {
-    border: 1px solid #ddd;
     padding: 16px;
-    border-radius: 12px;
     display: flex;
     flex-direction: column;
     gap: 12px;
